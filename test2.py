6def4df855fd0c40cb1a2183bdac8e74edd7c45b
Oobnala <alnboo1@gmail.com>
2019-04-30 20:22:41 -0700

    Added upsampling

--- test2.py
+++ test2.py
@@ -20,44 +20,50 @@ def f(row):
 
 
 def main():
-    data = pd.read_csv('student-mat.csv')
+    data = pd.read_csv('student-por.csv')
+    print(data.head())
     data.dropna(inplace=True)
 
-    df = data.iloc[:, [13, 14, 24, 25, 26, 27, 28, 29, 30, 31, 32]]
-    #print(df.head())
-    df['average'] = df.iloc[:, 8:11].astype(float).mean(axis=1)
+
+    df = data.iloc[:, [2, 13, 14, 24, 25, 26, 27, 28, 29, 30, 31, 32]]
+
+    df['average'] = df.iloc[:, 9:12].astype(float).mean(axis=1)
     df['pass/fail'] = df.apply(f,axis=1)
-    #df.loc[df['average']/20 > 0.64, 'pass/fail'] == 1
-    #df.loc[df['average']/20 <= 0.64, 'pass/fail'] == 0
-    #df['average'] = df['average'].astype(int)
-    #df['average'] = df['average'] + 1
+
     print(df.head())
-    #df.loc[df.index[11], 'average'] = average(df[8], df[9],df[10])
+
     print(len(df.columns))
     trainData, testData = train_test_split(df, test_size=0.3, random_state=1)
-    X_train = trainData.iloc[:, 0:7]
-    y_train = trainData.iloc[:, 12]
+
+    passing = trainData[trainData.iloc[:, -1] == 1]
+    failing = trainData[trainData.iloc[:, -1] == 0]
+
+    print(failing.count())
+
+    passingUpsample = resample(passing, replace=True, n_samples=310, random_state=1)
+
+    trainUpsample = pd.concat([passingUpsample, failing])
+
+    X_train = trainUpsample.iloc[:, 0:8].values
+    y_train = trainUpsample.iloc[:, -1].values
+
+
+    X_test = testData.iloc[:, 0:8].values
+    y_test = testData.iloc[:, -1].values
 
     nb = GaussianNB().fit(X_train, y_train)
-    X_test = testData.iloc[:, 0:7]
-    y_test = testData.iloc[:, 12]
     prediction = list(nb.predict(X_test))
-    print(prediction)
+    accuracy = round(nb.score(X_test, y_test) * 100, 2)
+    print('Python Accuracy for Naive Bayes: {}%'.format(accuracy))
 
     svm = SVC(kernel='linear').fit(X_train,y_train)
-    X_test = testData.iloc[:, 0:7]
-    y_test = testData.iloc[:, 12]
     prediction = list(svm.predict(X_test))
-    print(prediction)
     accuracy = round(svm.score(X_test,y_test) * 100, 2)
     print('Python SVM Accuracy: {}%'.format(accuracy))
 
     neighbors = KNeighborsClassifier(n_neighbors=3).fit(X_train, y_train)
     accuracy = round(neighbors.score(X_test, y_test) * 100, 2)
     print('Python KNN Accuracy: {}%'.format(accuracy))
-
-    accuracy = round(nb.score(X_test, y_test) * 100, 2)
-    print('Python Accuracy for Naive Bayes: {}%'.format(accuracy))
 
     logit = LogisticRegression(random_state=0, solver='lbfgs').fit(X_train, y_train)
     accuracy = round(logit.score(X_test, y_test) * 100, 2)
